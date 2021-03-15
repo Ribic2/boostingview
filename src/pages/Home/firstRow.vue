981d6e6ea26bf379ad7ec146cff72e4b877885b3
Vid Bukovec <vid.bukovec8361@gmail.com>
2021-03-15 20:23:13 +0100

    first commit

---
+++ src/pages/Home/firstRow.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-col align-self="center" cols="12">
+    <v-btn class="d-block ma-auto">Boost me</v-btn>
+  </v-col>
+</template>
+
+<script>
+export default {
+name: "firstRow"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+        v-model="toggle"
+        app
+        right
+    >
+      <h1>test</h1>
+    </v-navigation-drawer>
+
+   <v-app-bar app>
+
+     <v-toolbar-title>Boosting site</v-toolbar-title>
+     <v-spacer>
+
+     </v-spacer>
+
+     <!-- Breaks when viewport is mobile -->
+     <v-app-bar-nav-icon
+         v-if="$vuetify.breakpoint.xs"
+         @click="toggle = !toggle"
+     ></v-app-bar-nav-icon>
+
+     <!-- Displayed if viewport is not mobile-->
+     <div v-else>
+       <v-btn rounded :elevation="false">
+         Our prices
+       </v-btn>
+
+       <v-btn rounded :elevation="false">
+         Contact us
+       </v-btn>
+     </div>
+   </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <v-row class="row-fill">
+          <first-row></first-row>
+        </v-row>
+        <v-row class="row-fill">
+          <h1>test</h1>
+        </v-row>
+        <v-row class="row-fill">
+          <h1>test</h1>
+        </v-row>
+        <v-row class="row-fill">
+          <h1>test</h1>
+        </v-row>
+      </v-container>
+
+      <v-footer>
+
+      </v-footer>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+
+import FirstRow from "@/pages/Home/firstRow";
+export default {
+  name: 'Home',
+  components: {FirstRow},
+  data(){
+    return{
+      toggle: false
+    }
+  }
+}
+</script>
+
+<style>
+  .row-fill{
+    border: solid 1px black;
+    height: 100vh;
+  }
+</style>
